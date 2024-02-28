2a74bc7b7156932607554e9223f83a31006b9615
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2024-02-28 22:23:27 +0500

    Add datatypes-summary.js file

---
+++ datatypes-summary.js
@@ -0,0 +1,47 @@
+// Primitive
+
+// 7 types : String, Number, boolean, null, undefined, symbol(Unique Bnany ka liya),
+// BigInt
+
+//  const score = 100 
+//  const scoreValue= 100.3
+ 
+//  const isLoggedIn = false
+ const outsideTemp = null
+//  let userEmail;
+
+//  const id = Symbol('123')
+//  const anotherId = Symbol('123')
+
+//  console.log(id === anotherId);
+
+
+
+//  const bigNumber = 32435345354653543543654654n
+
+
+// IT IS A DYNAMICALLY TYPE LANGUAGE
+
+
+
+// Reference type (Non Primitive)
+
+// Array, Objects, Functions
+
+
+
+const heros = ["spiderman","batman","superman"];
+let myObj = {
+    name:"Usman",
+    age:20,
+    height:40,
+}
+
+// console.log(myObj)
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
