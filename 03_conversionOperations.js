e98841a4568cc0168cabcd905326a8f5d918754e
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2024-02-28 22:21:20 +0500

    Fix conversion and comparison operations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -45,9 +45,9 @@ let negValue = -value
 // console.log(2/2)
 // console.log(2%2)
 
-let str1 = "Hello"
-let str2 = " Usman"
-let str3 = str1 + str2
+// let str1 = "Hello"
+// let str2 = " Usman"
+// let str3 = str1 + str2
 // console.log(str3)
 
 // console.log("1" + 2)
@@ -57,15 +57,15 @@ let str3 = str1 + str2
 
 // console.log((1 + 2) * 3 % 2);
 
-console.log(true)
-console.log(+true)
-console.log(+"")
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
 
 
-let num1, num2, num3;
-num1 = num2 = num3 = 2 + 2;
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
 
-let gameCounter = 100;
-// gameCounter++;
-++gameCounter;
-console.log(gameCounter);
+// let gameCounter = 100;
+//  gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter);
