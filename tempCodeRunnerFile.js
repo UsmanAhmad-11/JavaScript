1a567a3393b8e28523aa434fb8decb728c82e37c
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2024-02-28 22:23:44 +0500

    Fix comparison bug in tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-let score = "Usman"
-
-// console.log(typeof score)
-// console.log(typeof (score))
-
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+console.log("1" > 2)
