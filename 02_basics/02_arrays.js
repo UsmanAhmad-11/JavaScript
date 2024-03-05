8bde0e149eda4153e3055a5a95e27f2ee761768b
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2024-03-05 22:55:42 +0500

    testing adding some console.log :)

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -18,17 +18,17 @@ const dc_heros = ["superman", "batman", "flash"]
 // const all_new_heroes = [...marvel_heros, ...dc_heros]
 // console.log(all_new_heroes)
 
-const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]] 
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]] 
 
-const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array)
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
 
 
-console.log(Array.isArray("Usman"))
-console.log(Array.from({name: "usman"})) //Interesting
+// console.log(Array.isArray("Usman"))
+// console.log(Array.from({name: "usman"})) //Interesting
 
-let score1 = 100;
-let score2 = 200;
-let score3 = 300;
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));
