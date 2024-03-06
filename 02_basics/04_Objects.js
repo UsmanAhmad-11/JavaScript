f7664fd5301c51d198465da1dc747e0554fd6cf8
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2024-03-06 23:23:55 +0500

    More on Objects

---
+++ 02_basics/04_Objects.js
@@ -0,0 +1,53 @@
+// const tinderUser = new Object()   // Singleton
+
+const tinderUser = {}                // Object Literals
+tinderUser.id = "123"
+tinderUser.name = "Usman"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "usman",
+            lastname : "ahmad"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+// const obj3 = {obj1, obj2}
+//  const obj4 = Object.assign({}, obj1, obj2, obj3)
+
+const obj4 = {...obj1, ...obj2, ...obj3}
+// console.log(obj4)
+
+const user = [
+    {
+        id : 1,
+        email : "h@gmail.com"
+    },
+    {
+        id : 1,
+        email : "h@gmail.com"
+    },
+    {
+        id : 1,
+        email : "h@gmail.com"
+    }
+]
+user[1].email;
+console.log(tinderUser)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLogged'))
