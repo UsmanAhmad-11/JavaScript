60358c1d04a4c85c65e9bb6f785444fa1cf10499
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2024-03-08 23:29:56 +0500

    Some Knowledge about JSON(Java Script Object Notation) & Desutructuring

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -30,24 +30,59 @@ const obj3 = {5: "e", 6: "f"}
 const obj4 = {...obj1, ...obj2, ...obj3}
 // console.log(obj4)
 
-const user = [
-    {
-        id : 1,
-        email : "h@gmail.com"
-    },
-    {
-        id : 1,
-        email : "h@gmail.com"
-    },
-    {
-        id : 1,
-        email : "h@gmail.com"
-    }
-]
-user[1].email;
-console.log(tinderUser)
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// const user = [
+//     {
+//         id : 1,
+//         email : "h@gmail.com"
+//     },
+//     {
+//         id : 1,
+//         email : "h@gmail.com"
+//     },
+//     {
+//         id : 1,
+//         email : "h@gmail.com"
+//     }
+// ]
+// user[1].email;
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.hasOwnProperty('isLogged'))
+
+
+// ____________________________________________________________________________________
+
+
+
+
+const course = {
+    coursename : "js in urdu",
+    price : "999",
+    courseInstructor : "Chai OR Code"   
+}
+
+// course.courseInstructor;
+
+ const {courseInstructor : instructor} = course;
+//  console.log(courseInstructor); 
+ console.log(instructor); 
+
+// DeStructuring
+//  const navbar = ({company}) => {
+
+//  }
+
+//  navbar(company = "usman")
+
+
+
+// JSON
+
+// {
+//     "name" : "Usman",
+//     "course" : "Java Script",
+//     "price" : "free"
+// }
