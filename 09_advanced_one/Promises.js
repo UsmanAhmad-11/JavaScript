4ea3ec3997edaca233b0c9e39778bbf9af8a697c
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2024-05-29 19:32:47 +0500

    some commenting for highlights

--- 09_advanced_one/Promises.js
+++ 09_advanced_one/Promises.js
@@ -1,88 +1,87 @@
-const promiseOne = new Promise(function(resolve, reject){
-    //Do an async task
-    //DB Calls, cryptography, network calls
-    setTimeout(function(){
-        console.log('Async Task is complete');
-        resolve();
-    }, 1000);
-})
+// FIRST METHOD
 
-promiseOne.then(function(){
-    console.log("Promise Consumed")
-})
+const promiseOne = new Promise(function (resolve, reject) {
+  //Do an async task
+  //DB Calls, cryptography, network calls
+  setTimeout(function () {
+    console.log("Async Task is complete");
+    resolve();
+  }, 1000);
+});
 
+promiseOne.then(function () {
+  console.log("Promise Consumed");
+});
 
+// SECOND METHOD
 
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    },1000)
-}).then(function(){
-    console.log("Async 2 Resolved");
-})
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 Resolved");
+});
+//THIRD METHOD
 
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Usman", email: "email@example.com" });
+  }, 1000);
+});
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-            resolve({username : "Usman", email:"email@example.com"})  
-    }, 1000)
-})
+promiseThree.then(function (user) {
+  console.log(user);
+});
+// 4th Method
 
-promiseThree.then(function(user){
-  console.log(user)  
-})
-
-
-const promiseFourth = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if(!error){
-            resolve({username:"Usman", password:"123"})
-        }else{
-            reject('Error: Something went wrong')
-        }
-
-    }, 1000)
-})
-
-
- promiseFourth.then((user)=>{
-    console.log(user);
-    return user.username
-}).then((username)=>{
-    console.log(username);
-}).catch((error)=>{
-    console.log(error)
-}).finally(()=> console.log("The Promised is either resolved or rejected"))
-
-
-
-const promiseFive = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if(!error){
-            resolve({username:"JavaScript", password:"123"})
-        }else{
-            reject('Error: JS went wrong')
-        }
-
-    }, 1000)
-})
-
-async function consumePromiseFive(){
-    try {
-        const response = await promiseFive
-        console.log(response);
-    } catch (error) {
-        console.log(error)
+    const promiseFourth = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "Usman", password: "123" });
+    } else {
+      reject("Error: Something went wrong");
     }
+  }, 1000);
+});
+
+promiseFourth
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => console.log("The Promised is either resolved or rejected"));
+//FIFTH METHOD
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "JavaScript", password: "123" });
+    } else {
+      reject("Error: JS went wrong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 consumePromiseFive();
-
-
-
 
 // async function getAllUsers(){
 //     try {
@@ -96,12 +95,11 @@ consumePromiseFive();
 
 // getAllUsers();
 
-
-fetch('https://api.github.com/users/hiteshchoudhary')
-.then((response)=>{
-    return response.json()
-})
-.then((data) => {
-    console.log(data)
-})
-.catch((error) => console.log(error))
+fetch("https://api.github.com/users/hiteshchoudhary")
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => console.log(error));
--- 09_advanced_one/tempCodeRunnerFile.js
+++ 09_advanced_one/tempCodeRunnerFile.js
@@ -1,1 +1,20 @@
-getAllUsers();
+const promiseOne = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         console.log('promise one consumed')
+//     },1000)
+//     resolve();
+// })
+// promiseOne.then(()=>{
+//     console.log('Promise one resolved');
+// })
+
+
+// // 2nd method
+// new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//      console.log('Promise two consumed');
+//      resolve();   
+//     }, 1000);
+// }).then(()=>{
+//     console.log('Promise two resolved')
+// })
